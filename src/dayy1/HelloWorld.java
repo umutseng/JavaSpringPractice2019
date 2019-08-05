109036c75f30f884d64bf1ad69a9581459066c58
umut <mtsngzr168@gmail.com>
2019-08-05 10:19:32 -0500

    second commit

--- src/dayy1/HelloWorld.java
+++ src/dayy1/HelloWorld.java
@@ -3,6 +3,8 @@ package dayy1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Hello world!");
 		System.out.println("Hello world!");
 
 	}
